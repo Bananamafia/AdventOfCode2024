5fa69b5941633e5ed33e3aabb10a091552f8306a
Bananamafia <63409017+Bananamafia@users.noreply.github.com>
2024-12-08 13:19:20 +0100

    Solution day04-2

--- src/day04.rs
+++ src/day04.rs
@@ -1,4 +1,5 @@
 pub mod day04 {
+    use std::collections::hash_map::Values;
 
     pub fn solve_part_1(input: &str) -> i32 {
         let values = get_values(input);
@@ -49,7 +50,35 @@ pub mod day04 {
     }
 
     pub fn solve_part_2(input: &str) -> i32 {
-        unimplemented!();
+        let values: Vec<Vec<char>> = get_values(input);
+
+        let mut solution: i32 = 0;
+
+        for y in 1..values.len() - 1 {
+            for x in 1..values[y].len() - 1 {
+                let current_char = values[y][x];
+
+                if current_char != 'A' {
+                    continue;
+                }
+
+                let neighbours = get_neighbours(x, y, &values);
+
+                if !has_more_than_one_of_m_and_s(&neighbours) {
+                    continue;
+                }
+
+                if has_more_than_one_of_m_and_s_flat(&get_corners_flat(&neighbours)) && diagonal_are_not_the_same(&neighbours) {
+                    solution += 1;
+                }
+
+                // if has_more_than_one_of_m_and_s_flat(&get_borders_flat(&neighbours)) {
+                //     solution += 1;
+                // }
+            }
+        }
+
+        solution
     }
 
     fn get_values(input: &str) -> Vec<Vec<char>> {
@@ -154,7 +183,11 @@ pub mod day04 {
         true
     }
 
-    fn check_diagonal_left_to_right_top_to_bottom(x: usize, y: usize, values: &Vec<Vec<char>>) -> bool {
+    fn check_diagonal_left_to_right_top_to_bottom(
+        x: usize,
+        y: usize,
+        values: &Vec<Vec<char>>,
+    ) -> bool {
         let mut current_char = values[y][x];
 
         if current_char != 'X' {
@@ -176,7 +209,11 @@ pub mod day04 {
         true
     }
 
-    fn check_diagonal_left_to_right_bottom_to_top(x: usize, y: usize, values: &Vec<Vec<char>>) -> bool {
+    fn check_diagonal_left_to_right_bottom_to_top(
+        x: usize,
+        y: usize,
+        values: &Vec<Vec<char>>,
+    ) -> bool {
         let mut current_char = values[y][x];
 
         if current_char != 'X' {
@@ -198,7 +235,11 @@ pub mod day04 {
         true
     }
 
-    fn check_diagonal_right_to_left_top_to_bottom(x: usize, y: usize, values: &Vec<Vec<char>>) -> bool {
+    fn check_diagonal_right_to_left_top_to_bottom(
+        x: usize,
+        y: usize,
+        values: &Vec<Vec<char>>,
+    ) -> bool {
         let mut current_char = values[y][x];
 
         if current_char != 'X' {
@@ -220,7 +261,11 @@ pub mod day04 {
         true
     }
 
-    fn check_diagonal_right_to_left_bottom_to_top(x: usize, y: usize, values: &Vec<Vec<char>>) -> bool {
+    fn check_diagonal_right_to_left_bottom_to_top(
+        x: usize,
+        y: usize,
+        values: &Vec<Vec<char>>,
+    ) -> bool {
         let mut current_char = values[y][x];
 
         if current_char != 'X' {
@@ -242,11 +287,50 @@ pub mod day04 {
         true
     }
 
+    fn get_neighbours(x: usize, y: usize, values: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+        if x < 1 || y < 1 || y > values.len() - 2 || x > values[0].len() - 2 {
+            panic!("Value is on boarder. Couldn't get all neighbours");
+        }
+
+        vec![
+            vec![values[y - 1][x - 1], values[y - 1][x], values[y - 1][x + 1]],
+            vec![values[y][x - 1], values[y][x], values[y][x + 1]],
+            vec![values[y + 1][x - 1], values[y + 1][x], values[y + 1][x + 1]],
+        ]
+    }
+
+    fn get_corners_flat(values: &Vec<Vec<char>>) -> Vec<char> {
+        vec![values[0][0], values[0][2], values[2][0], values[2][2]]
+    }
+
+    fn get_borders_flat(values: &Vec<Vec<char>>) -> Vec<char> {
+        vec![values[0][1], values[1][2], values[2][1], values[1][0]]
+    }
+
+    fn has_more_than_one_of_m_and_s(values: &Vec<Vec<char>>) -> bool {
+        let flat: Vec<char> = values.clone().into_iter().flat_map(|v: Vec<char>| v).collect::<Vec<char>>();
+        has_more_than_one_of_m_and_s_flat(&flat)
+    }
+
+    fn has_more_than_one_of_m_and_s_flat(values: &Vec<char>) -> bool {
+        values.iter().filter(|&v| v == &'M').count() > 1
+            && values.iter().filter(|&v| v == &'S').count() > 1
+    }
+
+    fn diagonal_are_not_the_same(values: &Vec<Vec<char>>) -> bool {
+        values[0][0] != values[2][2] && values[0][2] != values[2][0]
+    }
+
     #[cfg(test)]
     mod tests {
         use super::*;
 
-        const SAMPLE_INPUT: &str = "MMMSXXMASM
+        const SAMPLE_INPUT: &str = "..........
+MMMMMMMMMM
+.AAAAAAAA.
+SSSSSSSSSS
+..........
+MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
 MSAMASMSMX
@@ -267,6 +351,12 @@ MXMXAXMASX";
         }
 
         #[test]
-        fn solve_part_2_test() {}
+        fn solve_part_2_test() {
+            let expected: i32 = 17;
+
+            let actual: i32 = solve_part_2(SAMPLE_INPUT);
+
+            assert_eq!(expected, actual);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ mod day04;
 fn main() {
     let input = fs::read_to_string("inputs/day04.txt").expect("No file found");
 
-    print!("Solution: {}", day04::day04::solve_part_1(&input));
+    print!("Solution: {}", day04::day04::solve_part_2(&input));
 }
