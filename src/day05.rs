e086686329348fe26be043b6cbb4b5cbdac06936
Bananamafia <63409017+Bananamafia@users.noreply.github.com>
2024-12-22 00:13:56 +0100

    Solution day05-2

--- src/day05.rs
+++ src/day05.rs
@@ -1,5 +1,6 @@
 pub mod day05 {
     use std::collections::{HashMap, HashSet};
+    use std::ops::Index;
     use std::str::Split;
 
     pub fn get_ordering_rules(input: &str) -> Vec<(i32, i32)> {
@@ -13,7 +14,9 @@ pub mod day05 {
         rules
     }
 
-    pub fn get_clean_ordering_rules(rules: Vec<(i32, i32)>) -> HashMap<i32, Vec<i32>> {
+    pub fn get_clean_ordering_rules_ordered_by_last(
+        rules: &Vec<(i32, i32)>,
+    ) -> HashMap<i32, Vec<i32>> {
         let mut cleaned_rules: HashMap<i32, Vec<i32>> = HashMap::new();
 
         for rule in rules {
@@ -21,6 +24,21 @@ pub mod day05 {
                 .entry(rule.1)
                 .or_insert(Vec::new())
                 .push(rule.0);
+        }
+
+        cleaned_rules
+    }
+
+    pub fn get_clean_ordering_rules_ordered_by_first(
+        rules: &Vec<(i32, i32)>,
+    ) -> HashMap<i32, Vec<i32>> {
+        let mut cleaned_rules: HashMap<i32, Vec<i32>> = HashMap::new();
+
+        for rule in rules {
+            cleaned_rules
+                .entry(rule.0)
+                .or_insert(Vec::new())
+                .push(rule.1);
         }
 
         cleaned_rules
@@ -37,15 +55,15 @@ pub mod day05 {
         numbers
     }
 
-    pub fn is_update_in_order(update: &Vec<i32>, rules: &HashMap<i32, Vec<i32>>) -> bool  {
+    pub fn is_update_in_order(update: &Vec<i32>, rules: &HashMap<i32, Vec<i32>>) -> bool {
         let mut order_breaking_numbers: HashSet<i32> = HashSet::new();
 
         for value in update {
-            if order_breaking_numbers.contains(&value){
+            if order_breaking_numbers.contains(&value) {
                 return false;
             }
 
-            if rules.contains_key(&value){
+            if rules.contains_key(&value) {
                 let must_come_before: &Vec<i32> = rules.get(&value).unwrap();
                 order_breaking_numbers.extend(must_come_before);
             }
@@ -57,13 +75,13 @@ pub mod day05 {
     pub fn solve_part_1(input: &str) -> i32 {
         let input_parts: Vec<&str> = input.split("\n\n").collect();
         let rules: Vec<(i32, i32)> = get_ordering_rules(input_parts[0]);
-        let cleaned_rules: HashMap<i32, Vec<i32>> = get_clean_ordering_rules(rules);
+        let cleaned_rules: HashMap<i32, Vec<i32>> = get_clean_ordering_rules_ordered_by_last(&rules);
         let update_numbers: Vec<Vec<i32>> = get_update_numbers(input_parts[1]);
 
         let mut solution: i32 = 0;
 
         for update in update_numbers {
-            if is_update_in_order(&update, &cleaned_rules){
+            if is_update_in_order(&update, &cleaned_rules) {
                 let middle_index = update.len() / 2;
                 solution += update[middle_index]
             }
@@ -72,8 +90,47 @@ pub mod day05 {
         solution
     }
 
+    fn get_ordered_update(update: &Vec<i32>, rules: &HashMap<i32, Vec<i32>>) -> Vec<i32> {
+        let mut ordered_update: Vec<i32> = Vec::new();
+
+        for value in update.into_iter().rev() {
+
+            if rules.contains_key(value){
+                let rule = rules.get(value).unwrap();
+                let first_index = ordered_update.iter().position(|v| rule.contains(v));
+
+                ordered_update.insert(first_index.unwrap_or(ordered_update.len()), *value);
+            }
+            else {
+                ordered_update.push(*value);
+            }
+        }
+
+        ordered_update
+    }
+
     pub fn solve_part_2(input: &str) -> i32 {
-        0
+        let input_parts: Vec<&str> = input.split("\n\n").collect();
+        let rules: Vec<(i32, i32)> = get_ordering_rules(input_parts[0]);
+        let cleaned_rules_ordered_by_last: HashMap<i32, Vec<i32>> =
+            get_clean_ordering_rules_ordered_by_last(&rules);
+        let cleaned_rules_ordered_by_first: HashMap<i32, Vec<i32>> =
+            get_clean_ordering_rules_ordered_by_first(&rules);
+
+        let update_numbers: Vec<Vec<i32>> = get_update_numbers(input_parts[1]);
+
+        let mut solution: i32 = 0;
+
+        for update in update_numbers {
+            if !is_update_in_order(&update, &cleaned_rules_ordered_by_last) {
+                let ordered_update: Vec<i32> =
+                    get_ordered_update(&update, &cleaned_rules_ordered_by_first);
+                let middle_index = ordered_update.len() / 2;
+                solution += ordered_update[middle_index]
+            }
+        }
+
+        solution
     }
 
     #[cfg(test)]
@@ -105,7 +162,7 @@ pub mod day05 {
                 HashMap::from([(13, vec![97, 22]), (47, vec![97]), (29, vec![75])]);
 
             let actual: HashMap<i32, Vec<i32>> =
-                get_clean_ordering_rules(get_ordering_rules(input));
+                get_clean_ordering_rules_ordered_by_last(&get_ordering_rules(input));
 
             assert_eq!(expected, actual);
         }
@@ -128,8 +185,8 @@ pub mod day05 {
         }
 
         #[test]
-        fn test_ordered_update(){
-            let rules: HashMap<i32, Vec<i32>> = HashMap::from([(3, vec!(1, 2))]);
+        fn test_ordered_update() {
+            let rules: HashMap<i32, Vec<i32>> = HashMap::from([(3, vec![1, 2])]);
             let update: Vec<i32> = vec![1, 2, 3];
 
             let expected: bool = true;
@@ -140,13 +197,25 @@ pub mod day05 {
         }
 
         #[test]
-        fn test_unordered_update(){
-            let rules: HashMap<i32, Vec<i32>> = HashMap::from([(3, vec!(1, 2))]);
+        fn test_unordered_update() {
+            let rules: HashMap<i32, Vec<i32>> = HashMap::from([(3, vec![1, 2])]);
             let update: Vec<i32> = vec![3, 2, 1];
 
             let expected: bool = false;
 
             let actual: bool = is_update_in_order(&update, &rules);
+
+            assert_eq!(expected, actual);
+        }
+
+        #[test]
+        fn test_get_ordered_update() {
+            let rules: HashMap<i32, Vec<i32>> = HashMap::from([(1, vec![2, 3]), (2, vec![3])]);
+            let update: Vec<i32> = vec![3, 2, 1];
+
+            let expected: Vec<i32> = vec![1, 2, 3];
+
+            let actual: Vec<i32> = get_ordered_update(&update, &rules);
 
             assert_eq!(expected, actual);
         }
@@ -190,6 +259,12 @@ pub mod day05 {
         }
 
         #[test]
-        fn test_solve_part_2() {}
+        fn test_solve_part_2() {
+            let expected: i32 = 123;
+
+            let actual: i32 = solve_part_2(SAMPLE_INPUT);
+
+            assert_eq!(expected, actual);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,5 @@ mod day05;
 fn main() {
     let input = fs::read_to_string("inputs/day05.txt").expect("No file found");
 
-    print!("Solution: {}", day05::day05::solve_part_1(&input));
+    print!("Solution: {}", day05::day05::solve_part_2(&input));
 }
